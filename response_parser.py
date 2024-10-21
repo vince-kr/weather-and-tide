70834fce90f5ed5756cbfb1cc98adf1fd63ebb8b
Vince <vince@krijnsen.com>
2024-10-21 20:23:22 +0100

    add tests+code to extract weather data

--- response_parser.py
+++ response_parser.py
@@ -1,3 +1,6 @@
+from collections import Counter, namedtuple
+from collections.abc import Iterable
+import itertools
 """
 All of the below may be useful once I start implementing weather
 import xml.etree.ElementTree as ET
@@ -16,18 +19,49 @@ class Forecast:
         self.temp_unit = forecast[0][0].attrib['unit']
         self.wind_speed = forecast[0][2].attrib['mps']
 """
-import apis
 
+Selector = namedtuple('Selector', ['data_type', 'key', 'symbol'])
 
 def format_tide_response(tides: dict) -> tuple:
-    rows = tuple(
+    return tuple(
         (
             tide_datum['type'].capitalize(),
             tuple((_extract_time_from(tide_datum['time']),))
         )
         for tide_datum in tides['data']
     )
-    return rows
 
 def _extract_time_from(datetimestamp: str) -> str:
     return datetimestamp[11:16]
+
+def format_weather_response(weather: dict) -> tuple:
+    forecasts = weather['weatherdata']['product']['time']
+    weather, precip = _extract_weather_datums(forecasts[:26])
+    weather_batches = itertools.batched(weather, n=3)
+    precip_batches = itertools.batched(precip, n=3)
+    for weather_point, precip_point in zip(weather_batches, precip_batches):
+        pass
+
+
+def _extract_weather_datums(all_data: Iterable) -> tuple:
+    """Given an iterable, return a 2-tuple of tuples that contain
+    - all even indices from the iterable minus the last one
+    - all odd indices from the iterable minus the first one
+    """
+    evens = tuple(point['location'] for idx, point in enumerate(all_data[:-2])
+                  if _is_even(idx))
+    odds = tuple(point['location'] for idx, point in enumerate(all_data[2:])
+                 if not _is_even(idx))
+    return evens, odds
+
+def _is_even(number: int) -> bool:
+    return number % 2 == 0
+
+def _average_value(data: tuple, selector: Selector) -> str:
+    pertinent_values = [point[selector.data_type][selector.key]
+                        for point in data]
+    try:
+        val = sum(float(point) for point in pertinent_values) / len(data)
+    except ValueError:
+        val = Counter(pertinent_values).most_common()[0][0]
+    return f'{val}{selector.symbol}'
--- tests/test_response_parser.py
+++ tests/test_response_parser.py
@@ -2,6 +2,7 @@
 import json
 import response_parser
 import unittest
+import xmltodict
 
 
 class TestTide(unittest.TestCase):
@@ -35,3 +36,207 @@ class TestTide(unittest.TestCase):
         )
         actual = response_parser.format_tide_response(self.response)
         self.assertEqual(expected, actual)
+
+
+class TestWeather(unittest.TestCase):
+    def setUp(self):
+        with open('cached_api_responses/weather.xml') as wf:
+            self.response: dict = xmltodict.parse(wf.read())
+        self.expected_formatting = (
+            (
+                "9.1° C",
+                "9.2° C",
+                "8.3° C",
+                "7.5° C",
+            ),
+            (
+                "0 mm",
+                "0.7 mm",
+                "1.7 mm",
+                "2.2 mm",
+            ),
+            (
+                "24.6 km/h",
+                "25.2 km/h",
+                "31.1 km/h",
+                "36.2 km/h",
+            ),
+            (
+                "N",
+                "N",
+                "N",
+                "N",
+            ),
+            (
+                "98.5%",
+                "100.0",
+                "100.0",
+                "100.0",
+            ),
+        )
+        self.input_data = (
+            (
+                {
+                    "temperature": {
+                        "@unit": "12.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+                {
+                    "temperature": {
+                        "@unit": "13.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+                {
+                    "temperature": {
+                        "@unit": "14.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+            ),
+            (
+                {
+                    "temperature": {
+                        "@unit": "12.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+                {
+                    "temperature": {
+                        "@unit": "13.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+                {
+                    "temperature": {
+                        "@unit": "14.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+            ),
+            (
+                {
+                    "temperature": {
+                        "@unit": "12.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+                {
+                    "temperature": {
+                        "@unit": "13.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+                {
+                    "temperature": {
+                        "@unit": "14.4"
+                    },
+                    "windDirection": {
+                        "@name": "N"
+                    }
+                },
+            )
+        )
+
+    def test_sanity(self):
+        self.assertTrue(True)
+
+    def test_givenResponseAsDict_returnPertinentDataPoints(self):
+        input_data = [{"location": count} for count in range(10)]
+        expected = (0, 2, 4, 6), (3, 5, 7, 9)
+        actual = response_parser._extract_weather_datums(input_data)
+        self.assertEqual(expected, actual)
+
+    def test_givenThrupleOfData_returnAverageOfNumericalFields(self):
+        selector = response_parser.Selector(
+            data_type="temperature",
+            key="@unit",
+            symbol="° C"
+        )
+        input_thruple = self.input_data[0]
+        expected = "13.4° C"
+        actual = response_parser._average_value(input_thruple, selector)
+        self.assertEqual(expected, actual)
+
+    def test_givenThrupleOfData_returnMostCommonString(self):
+        selector = response_parser.Selector(
+            "windDirection",
+            "@name",
+            ""
+        )
+        input_thruple = self.input_data[0]
+        expected = "N"
+        actual = response_parser._average_value(input_thruple, selector)
+        self.assertEqual(expected, actual)
+
+    def test_givenCollectionOfThruplesOfData_returnTupleOfValues(self):
+        selector = response_parser.Selector(
+            "temperature",
+            "@unit",
+            "° C"
+        )
+        expected = (
+            "13.4° C",
+            "13.4° C",
+            "13.4° C",
+        )
+        actual = tuple(response_parser._average_value(batch, selector)
+                       for batch in self.input_data)
+        self.assertEqual(expected, actual)
+
+    def test_givenMultipleSelectors_returnMultipleTuples(self):
+        selectors = (
+            response_parser.Selector(
+                "temperature",
+                "@unit",
+                "° C"
+            ),
+            response_parser.Selector(
+                "windDirection",
+                "@name",
+                ""
+            )
+        )
+        expected = (
+            (
+                "13.4° C",
+                "13.4° C",
+                "13.4° C",
+            ),
+            (
+                "N",
+                "N",
+                "N",
+            )
+        )
+        results = []
+        for sel in selectors:
+            results.append(tuple(response_parser._average_value(batch, sel)
+                                 for batch in self.input_data))
+        actual = tuple(results)
+        self.assertEqual(expected, actual)
+
+    def SKIPtest_wthXML(self):
+        """Build a JSON representation of weather data"""
+        forecasts = self.response['weatherdata']['product']['time']
+        weather, precip = response_parser._extract_weather_datums(forecasts[:26])
+        print(weather)
+        with open('really.json', 'w') as rf:
+            json.dump(weather, rf, indent=4)
