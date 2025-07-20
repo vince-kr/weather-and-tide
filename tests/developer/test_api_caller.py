b8f773a484935f945fc99fd5f857eb67934765fa
Vince K <vince@krijnsen.com>
2025-07-20 06:58:35 +0100

    move to pytest
    This commit adds the pytest package to requirements and rewrites test_api_caller.py in pytest syntax.

--- tests/developer/test_api_caller.py
+++ tests/developer/test_api_caller.py
@@ -1,42 +1,45 @@
 import datetime
-import unittest
+
+import pytest
 
 import api_caller
 
-class TestApiCaller(unittest.TestCase):
-    def setUp(self):
-        date_today = datetime.date.today()
-        self.start_of_today = date_today.isoformat() + 'T00:00:00'
-        date_tomorrow = date_today + datetime.timedelta(days=1)
-        self.start_of_tomorrow = date_tomorrow.isoformat() + 'T00:00:00'
+@pytest.fixture
+def dates():
+    date_today = datetime.date.today()
+    date_tomorrow = date_today + datetime.timedelta(days=1)
+    return {
+        "start_of_today": date_today.isoformat() + 'T00:00:00',
+        "start_of_tomorrow": date_tomorrow.isoformat() + 'T00:00:00'
+    }
 
-    def test_sanity(self):
-        self.assertTrue(True)
+@pytest.fixture
+def coords():
+    return 53.2048, -6.0979
 
-    def test_generateWeatherRequestObject(self):
-        coords = (53.2048, -6.0979)
-        base_url = 'http://some-fake-url.com/weather'
-        expected = {
-            'url': base_url + f'?lat=53.2048;long=-6.0979'
+def test_generate_weather_request_object(coords):
+    base_url = 'http://some-fake-url.com/weather'
+    expected = {
+        'url': base_url + f'?lat=53.2048;long=-6.0979'
+    }
+    actual = api_caller._build_weather_request(base_url, coords)
+    assert expected == actual
+
+def test_generate_tide_request_object(dates, coords):
+    base_url = 'http://some-fake-url.com/tides'
+    lat, long = coords
+    api_key = 'not-really-an-api-key'
+    expected = {
+        'url': base_url,
+        'params': {
+            'lat': lat,
+            'lng': long,
+            'start': dates['start_of_today'],
+            'end': dates['start_of_tomorrow']
+        },
+        'headers': {
+            'Authorization': api_key
         }
-        actual = api_caller._build_weather_request(base_url, coords)
-        self.assertEqual(expected, actual)
-
-    def test_generateTideRequestObject(self):
-        base_url = 'http://some-fake-url.com/tides'
-        lat, long = (53.2048, -6.0979)
-        api_key = 'not-really-an-api-key'
-        expected = {
-            'url': base_url,
-            'params': {
-                'lat': lat,
-                'lng': long,
-                'start': self.start_of_today,
-                'end': self.start_of_tomorrow
-            },
-            'headers': {
-                'Authorization': api_key
-            }
-        }
-        actual = api_caller._build_tide_request(base_url, (lat, long), api_key)
-        self.assertEqual(expected, actual)
+    }
+    actual = api_caller._build_tide_request(base_url, (lat, long), api_key)
+    assert expected == actual
